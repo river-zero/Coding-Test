fde7f5359abb8b0aa48f8a73d4fd443a043889a9
river-zero <124229424+river-zero@users.noreply.github.com>
2023-12-30 19:54:32 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 2.42 ms, Memory: 5.29 MB -BaekjoonHub

---
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+#include <map>
+using namespace std;
+
+vector<int> solution(string s) {
+    vector<int> answer;
+    map<char, int> m;
+    
+    for(int i = 0; i < s.size(); i++) {
+        
+        if(m.find(s[i]) == m.end()) {
+            answer.push_back(-1); 
+            m.insert({s[i], i});
+        } else {
+            answer.push_back(i - m[s[i]]);
+            m[s[i]] = i;
+        }
+    }
+    
+    return answer;
+}
