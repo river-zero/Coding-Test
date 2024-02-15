ff58dd79888b0a9bc00690340c926f0174119d16
river-zero <124229424+river-zero@users.noreply.github.com>
2024-02-15 18:09:27 +0900

    [Bronze V] Title: A+B - 5, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze V/10952. A＋B － 5/A＋B － 5.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    while (true) {
+        int A, B;
+        cin >> A >> B;
+        if (A == 0 && B == 0) break;
+        cout << A + B << endl;
+    }
+}
