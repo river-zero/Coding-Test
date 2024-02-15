87f427de642d20e0e01884acf300483d7e762a6d
river-zero <124229424+river-zero@users.noreply.github.com>
2024-02-15 16:41:29 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze V/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int A;
+    int B;
+    cin >> A >> B;
+    
+    cout << A + B << endl;
+    cout << A - B << endl;
+    cout << A * B << endl;
+    cout << A / B << endl;
+    cout << A % B << endl;
+}
