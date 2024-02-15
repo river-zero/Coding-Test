68a141708007295f6a21b415f1b361d1342fb396
river-zero <124229424+river-zero@users.noreply.github.com>
2024-02-15 15:27:06 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze V/2438. 별 찍기 － 1/별 찍기 － 1.cc
+++ 백준/Bronze V/2438. 별 찍기 － 1/별 찍기 － 1.cc
@@ -8,9 +8,6 @@ int main() {
     cin >> n;
     
     for (int i = 1; i <= n; i++) {
-        for (int j = 0; j < i; j++) {
-           cout << '*'; 
-        }
-        cout << endl;
+        cout << string(i, '*') << endl;
     }
 }
