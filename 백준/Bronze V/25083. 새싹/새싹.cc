7de8aee017f194466c8d31827067d62eecd0f3f3
river-zero <124229424+river-zero@users.noreply.github.com>
2024-02-15 18:25:26 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze V/25083. 새싹/새싹.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "         ,r'\"7\n"
+            "r`-_   ,'  ,/\n"
+            " \\. \". L_r'\n"
+            "   `~\\/ \n"
+            "      |\n"
+            "      |";
+    
+    // 원시 문자열 리터럴을 사용한다면
+    // R"(여기에 원하는 내용 그대로)"
+}
