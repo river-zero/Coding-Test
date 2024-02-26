42bbce8863c92e65a80bd7b1cf35d795fc4d6247
river-zero <124229424+river-zero@users.noreply.github.com>
2024-02-26 22:51:30 +0900

    [Silver II] Title: 최소 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    
+    int N;
+    cin >> N;
+    
+    priority_queue<int, vector<int>, greater<int>> pq;
+    
+     while (N--) {
+        int x;
+        cin >> x;
+
+        if (x == 0) {
+            if (pq.empty()) cout << "0\n";
+            else {
+                cout << pq.top() << "\n";
+                pq.pop();
+            }
+        } else {
+            pq.push(x);
+        }
+    }
+}
