826f562aea357a992dad540ee91e8aa7c12f1ce1
river-zero <124229424+river-zero@users.noreply.github.com>
2024-06-06 11:27:22 +0900

    [Silver II] Title: 최대 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int N;
+    cin >> N;
+    
+    priority_queue<int> pq;
+    
+    while (N--) {
+        int x;
+        cin >> x;
+        
+        if (x == 0) {
+            if (pq.empty()) {
+                cout << "0\n";
+            } else {
+                cout << pq.top() << '\n';
+                pq.pop();
+            }
+        } else {
+            pq.push(x);
+        }
+    }
+    
+    return 0;
+}
